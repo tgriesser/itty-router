9d3f72ac621f98621e01cf66007375c0db675470
Tim Griesser <tgriesser10@gmail.com>
2022-08-30 08:28:00 -0400

    Correct typing for res on onResponse

--- dist/itty-router.d.ts
+++ dist/itty-router.d.ts
@@ -101,7 +101,7 @@ export declare type IttyRouterDefinition = [
 export interface IttyRouterConfig {
   base?: string;
   routes?: IttyRouterDefinition[];
-  onResponse?: (req: Request, res: unknown) => Response
+  onResponse?: <Req extends IttyGenericRequest = Request>(req: Req, res: unknown) => Response
 }
 
 export interface IttyRouterTypeConfig {
